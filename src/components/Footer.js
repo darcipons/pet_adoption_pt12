5006cbdcd53f878f9f292c731bdf30d3aabaec98
darcipons <darcimarie33@gmail.com>
2020-04-17 16:41:05 -0400

    updated navbar background colo, changed header image, removed home styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,31 +1,14 @@
-import React, { Component } from 'react';
-import { Nav } from 'react-bootstrap/'
+import React from 'react';
 import './Footer.css'
-import dogsAndCats from '../images/dogsAndCatsRow.jpg'
 
-class Footer extends Component {
-    render() {
-        return (
-            <div className="content">
-              <Nav className="mr-auto color-nav">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/about">About Us</Nav.Link>
-                <Nav.Link href="/master">Pet Lookup</Nav.Link>
-                <Nav.Link href="/details">Pet Details</Nav.Link>
-                <Nav.Link href="/visit">Schedule Visit</Nav.Link>
-                <Nav.Link href="/resources">Resources</Nav.Link>
-                <img
-                  alt=""
-                  src={dogsAndCats}
-                  width="162"
-                  height="75"
-                  className="d-inline-block align-top"
-                  />{' '}
-                </Nav>
-                <div className="push"></div>
-            </div>
-        );
-    }
+const Footer = () => {
+  return (
+    <div>
+      <footer>
+        <div>Copyright 2020</div>
+      </footer>
+    </div>
+  )
 }
 
 export default Footer;
